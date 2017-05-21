32a8a819982b3265c94000312d0064c9f170a3f4
hellokarvel <hellokarvel@163.com>
2017-05-21 23:29:06 +0800

    Add files via upload

---
+++ FJOB.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+//scanf("%s",b);          //可以输入一串不超过20字符的字符串。
+//%3.0f表示至少占三个字宽，且不带小数点和小数部分。%6.1f表示至少6个字宽，
+ //	小数点后1位 getchar putchar 组合使用
+int main(){
+	char c='f';
+	int b =102;
+	char a[10]="ABCD";
+	printf("%s\n",a);
+	printf("%d\n",c);
+  printf("%c\n",b);
+}
