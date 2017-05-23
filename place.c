e95a8381b1e7fe1a9794ad559e7982fb94ed7ed8
hellokarvel <hellokarvel@163.com>
2017-05-23 17:29:28 +0800

    Add files via upload

---
+++ place.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+//scanf("%s",b);          //可以输入一串不超过20字符的字符串。
+//%3.0f表示至少占三个字宽，且不带小数点和小数部分。%6.1f表示至少6个字宽，
+ //	小数点后1位 getchar putchar 组合使用
+// 反斜杠\a 警告　\b 退格　\f 换页　\n 换行　\r 回车\t 跳格　\v 垂直跳格
+int main(){
+int c = getchar();
+    while(1) {
+    	 
+    	      if(c !=' ')printf("%c",c);				//c    abc abc  abc
+												
+    	       if( c ==' ')                                      
+    	        if(a !=' ')putchar(c);
+    	        a=c;
+    	        
+    	  // printf("\tIt is valum a:%c\n",a);
+		 }
+    printf("\thello");
+  //  printf("hello");
+}
